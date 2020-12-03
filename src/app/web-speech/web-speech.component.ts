4835b0ff79567d00275d50d0fa9ebb22d19fb128
Samuel-dot-cloud <successam336@gmail.com>
2020-12-03 20:25:07 +0300

    Creating the app html template and a speech synthesizer service

--- src/app/web-speech/web-speech.component.ts
+++ src/app/web-speech/web-speech.component.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { merge, Observable, Subject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { defaultLanguage, languages } from '../shared/model/languages';
+import { SpeechError } from '../shared/model/speech-error';
+import { SpeechEvent } from '../shared/model/speech-event';
 import { SpeechRecognizerService } from '../shared/services/web-apis/speech-recognizer.service';
 
 @Component({
@@ -12,13 +15,13 @@ import { SpeechRecognizerService } from '../shared/services/web-apis/speech-reco
 export class WebSpeechComponent implements OnInit {
   languages: string[] = languages;
   currentLanguage: string = defaultLanguage; //Set the default language
-  totalTranscription!: string; // The variable to accumulate all the recognized texts
+  totalTranscript!: string; // The variable to accumulate all the recognized texts
 
   transcript$!: Observable<string>; // Shows the transcript in "real-time"
   listening$!: Observable<boolean>; // Changes to 'true'/'false' when the recognizer starts/stops
   errorMessage$!: Observable<string>; // An error from the Speech Recognizer
   defaultError$ = new Subject<undefined>(); // Clean-up of the previous errors
-  
+
 
   constructor(private speechRecognizer: SpeechRecognizerService) { }
 
@@ -49,6 +52,62 @@ export class WebSpeechComponent implements OnInit {
     }
     this.currentLanguage = language;
     this.speechRecognizer.setLanguage(this.currentLanguage);
+  }
+
+  private initRecognition(): void {
+
+    // "transcript$" now will receive every text(interim result) from the Speech API.
+    //  Also, for every "Final Result"(from the speech), the code will append that text to the existing Text Area component.
+    this.transcript$ = this.speechRecognizer.onResult().pipe(
+      tap((notification) => {
+        if (notification.event === SpeechEvent.FinalContent) {
+          this.totalTranscript = this.totalTranscript
+            ? `${this.totalTranscript}\n${notification.content?.trim()}` : notification.content;
+        }
+      }),
+      map((notification) => notification.content || '')
+    );
+
+    // "listening$" will receive 'true' when the Speech API starts and 'false' when it's finished.
+    this.listening$ = merge(
+      this.speechRecognizer.onStart(),
+      this.speechRecognizer.onEnd()
+    ).pipe(
+      map((notification) => notification.event === SpeechEvent.Start)
+    );
+
+    // "errorMessage$" will receive any error from Speech API and it will map that value to a meaningful message for the user
+    this.errorMessage$ = merge(
+      this.speechRecognizer.onError(),
+      this.defaultError$
+    ).pipe(
+      map((data) => {
+        if (data === undefined) {
+          return '';
+        }
+        let message;
+        switch (data.error) {
+          case SpeechError.NotAllowed:
+            message = `Cannot run the demo. 
+            Your browser is not authorized to access your microphone. 
+            Verify that your browser has access to your microphone and try again.`;
+            break;
+
+          case SpeechError.NoSpeech:
+            message = `No sppech has been detected. Please try again.`;
+            break;
+
+          case SpeechError.AudioCapture:
+            message = `Microphone is not available. Please verify the connection of your microphone and try again.`;
+            break;
+
+          default:
+            message = '';
+            break;
+        }
+        return message;
+      })
+    );
   }
 
 }
