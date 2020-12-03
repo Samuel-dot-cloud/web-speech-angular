e681ae209de5ab40e84d75e250489dc5746ac302
Samuel-dot-cloud <successam336@gmail.com>
2020-12-03 16:47:27 +0300

    Implementing observables for a continuous data flow

--- src/app/shared/services/web-apis/speech-recognizer.service.ts
+++ src/app/shared/services/web-apis/speech-recognizer.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SpeechError } from '../../model/speech-error';
+import { SpeechEvent } from '../../model/speech-event';
+import { SpeechNotification } from '../../model/speech-notification';
 
 @Injectable({
   providedIn: 'root'
@@ -29,5 +33,81 @@ export class SpeechRecognizerService {
 
   stop(): void {
     this.recognition.stop();
+  }
+
+  onStart() : Observable<SpeechNotification<never>> {
+    if(!this.recognition){
+      this.initialize(this.language);
+    }
+
+    return new Observable(observer => {
+      this.recognition.onstart = () => observer.next({
+        event: SpeechEvent.Start
+      });
+    });
+  }
+
+  onEnd() : Observable<SpeechNotification<never>> {
+    return new Observable(observer => {
+      this.recognition.onend = () => {
+        observer.next({
+          event: SpeechEvent.End
+        });
+        this.isListening = false;
+      };
+    });
+  }
+
+  onResult() : Observable<SpeechNotification<string>> {
+    return new Observable(observer => {
+      this.recognition.onresult = (event: SpeechRecognitionEvent) => {
+        let interimContent = '';
+        let finalContent = '';
+
+        for (let i = event.resultIndex; i< event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            finalContent += event.results[i][0].transcript;
+            observer.next({
+              event: SpeechEvent.FinalContent,
+              content: finalContent
+            });
+          } else {
+            interimContent += event.results[i][0].transcript;
+            observer.next({
+              event: SpeechEvent.InterimContent,
+              content: interimContent
+            });
+          }
+        }
+      };
+    });
+  }
+
+  onError(): Observable<SpeechNotification<never>> {
+    return new Observable(observer => {
+      this.recognition.onerror = (event) => {
+        const eventError: string = (event as any).error;
+        let error: SpeechError;
+        switch (eventError) {
+          case 'no-speech':
+            error = SpeechError.NoSpeech;
+            break;
+            case 'audio-capture':
+            error = SpeechError.AudioCapture;
+            break;
+            case 'not-allowed':
+            error = SpeechError.NotAllowed;
+            break;
+        
+          default:
+            error = SpeechError.Unknown;
+            break;
+        }
+
+        observer.next({
+          error
+        });
+      };
+    });
   }
 }
