8dd3bf6994066b74f5c83bf4a8462d20138c55be
Samuel-dot-cloud <successam336@gmail.com>
2020-12-03 16:17:58 +0300

    Creating the SpeechRecognizerService

--- src/app/shared/services/web-apis/speech-recognizer.service.ts
+++ src/app/shared/services/web-apis/speech-recognizer.service.ts
@@ -4,6 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class SpeechRecognizerService {
+  recognition!: SpeechRecognition;
+  language!: string;
+  isListening = false;
 
   constructor() { }
+
+  initialize(language: string): void {
+    this.recognition = new webkitSpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.interimResults = true;
+    this.setLanguage(language);
+  }
+
+  setLanguage(language: string): void {
+    this.language = language;
+    this.recognition.lang = language;
+  }
+
+  start(): void {
+    this.recognition.start();
+    this.isListening = true;
+  }
+
+  stop(): void {
+    this.recognition.stop();
+  }
 }
