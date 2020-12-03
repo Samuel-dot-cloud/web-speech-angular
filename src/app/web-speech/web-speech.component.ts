421af9fb618dc02bb0b3e4e6722394bc0174b01b
Samuel-dot-cloud <successam336@gmail.com>
2020-12-03 12:07:15 +0300

    Project initialization

---
+++ src/app/web-speech/web-speech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wsa-web-speech',
+  templateUrl: './web-speech.component.html',
+  styleUrls: ['./web-speech.component.css']
+})
+export class WebSpeechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
